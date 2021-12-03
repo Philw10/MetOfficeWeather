9cdc23235a32c507bd2cf390a82471f8e9c4be85
Phil Wilson <94173898+Philw10@users.noreply.github.com>
2021-12-03 13:24:47 +0000

    Add files via upload

--- src/main/java/training/metofficeweather/WeatherApplication.java
+++ src/main/java/training/metofficeweather/WeatherApplication.java
@@ -2,12 +2,24 @@ package training.metofficeweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
 
 @SpringBootApplication
 public class WeatherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
+
+		Client client = ClientBuilder.newClient();
+		String name = client.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=724c591c-bbe6-497f-bbbf-1cd3effa45ee")
+				.request(MediaType.TEXT_PLAIN)
+				.get(String.class);
+
+		System.out.println(name);
+
 	}
 
 }
